24b5f22a9edff56fb2c3ebe27dcd12f136bc2207
visseho <visseho.adjiwanou@uct.ac.za>
2018-12-29 17:31:36 -0500

    Intro 1 modifié

--- imqcss/Intro_1/initLesson.R
+++ imqcss/Intro_1/initLesson.R
@@ -3,7 +3,9 @@
 # the user's working directory and thus be accessible to them
 # throughout the lesson.
 
-setwd(dir="C:/Users/Micro Morue/Desktop/Cours/R/Swirl/imqcss/Intro_1")
+#setwd(dir="C:/Users/Micro Morue/Desktop/Cours/R/Swirl/imqcss/Intro_1")
+setwd(dir="C:/Users/visseho/Documents/Swirl_imqcss/imqcss/Intro_1")
+
 
 UNpop <- read.csv2("UNpop.csv", header = T)
 
