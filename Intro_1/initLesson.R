c946d4d0c2ea89bcbf8f01ba24432d8b6a9d96e1
Vissého Adjiwanou <visseho09@yahoo.fr>
2018-12-29 21:04:22 -0500

    change initLesson to Kosuke

--- Intro_1/initLesson.R
+++ Intro_1/initLesson.R
@@ -4,11 +4,21 @@
 # throughout the lesson.
 
 #setwd(dir="C:/Users/Micro Morue/Desktop/Cours/R/Swirl/imqcss/Intro_1")
-setwd(dir="/Users/visseho/Documents/swirl_imqcss/Intro_1")
+#setwd(dir="/Users/visseho/Documents/swirl_imqcss/Intro_1")
 
 
-UNpop <- read.csv2("UNpop.csv", header = T)
+#UNpop <- read.csv2("UNpop.csv", header = T)
 
-world.pop <- UNpop$world.pop
+#world.pop <- UNpop$world.pop
+
+#x <- sample(1:100, 10)
+
+
+
+swirl_options(swirl_logging = TRUE)
+
+year <- seq(from = 1950, to = 2010, by = 10)
+world.pop <- c(2525779, 3026003, 3691173, 4449049, 5320817, 6127700, 6916183)
+UNpop <- as.data.frame(cbind(world.pop, year))
 
 x <- sample(1:100, 10)
