e59ca0a80fed83eeb42123ae0218f33574f20bf7
visseho <visseho.adjiwanou@uct.ac.za>
2018-12-30 23:38:53 -0500

    Intro 2

--- imqcss/Causalite1/initLesson.R
+++ imqcss/Causalite1/initLesson.R
@@ -2,15 +2,3 @@
       # lesson is started. Any variables created here will show up in
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
-
-#library(swirl)
-
-# Chemin vers le répertoire du fichier
-lesson_dir <- file.path(path.package(substring(find("swirl")[1], 9)), "Courses",
-                        "qss-swirl", "CAUSALITY1")
-
-#Appeler le fichier, le nommer pour l'exercice
-data_path <- file.path(lesson_dir, "resume.csv")
-
-#Utiliser la commande 'read.csv()
-resume <- read.csv(data_path, stringsAsFactors=FALSE)
--- imqcss/Causalite2/initLesson.R
+++ imqcss/Causalite2/initLesson.R
@@ -2,15 +2,3 @@
       # lesson is started. Any variables created here will show up in
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
-
-# Make path to lesson directory
-lesson_dir <- file.path(path.package(substring(find("swirl")[1], 9)), "Courses",
-                        "qss-swirl", "CAUSALITY2")
-
-# Make path to data and let user call read.csv(data_path)
-data_path <- file.path(lesson_dir, "resume.csv")
-data_path2 <- file.path(lesson_dir, "social.csv")
-
-# Load data into a variable for the user
-resume <- read.csv(data_path, stringsAsFactors=FALSE)
-social <- read.csv(data_path2, stringsAsFactors=FALSE)
