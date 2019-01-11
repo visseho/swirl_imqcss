3c5ec065f7dc726a5057d0bafea3772b2ce71dca
visseho <visseho.adjiwanou@uct.ac.za>
2019-01-11 14:19:26 -0500

    chemin enleve

--- Intro_1/initLesson.R
+++ Intro_1/initLesson.R
@@ -6,28 +6,29 @@
 #setwd(dir="C:/Users/Micro Morue/Desktop/Cours/R/Swirl/imqcss/Intro_1")
 #setwd(dir="/Users/visseho/Documents/swirl_imqcss/Intro_1")
 
-UNpop <- read.csv2("UNpop.csv", header = T)
+#UNpop <- read.csv2("UNpop.csv", header = T)
 
-world.pop <- UNpop$world.pop
+#world.pop <- UNpop$world.pop
 
-x <- sample(1:100, 10)
+#x <- sample(1:100, 10)
 
 
 # Make path to lesson directory
 #lesson_dir <- file.path(path.package(substring(find("swirl")[1], 9)), "Courses",
 #                        "qss-swirl", "INTRO2")
 
-lesson_dir <- file.path(path.package(substring(find("swirl")[1], 9)), "Courses",
-                        "swirl_imqcss", "Intro_1")
+#lesson_dir <- file.path(path.package(substring(find("swirl")[1], 9)), "Courses",
+#                        "swirl_imqcss", "Intro_1")
 
 # Make path to data and let user call read.csv(data_path)
-data_path <- file.path(lesson_dir, "data.csv")
+#data_path <- file.path(lesson_dir, "data.csv")
 
+# Another way, from Kosuke
 
-#swirl_options(swirl_logging = TRUE)
+swirl_options(swirl_logging = TRUE)
 
-#year <- seq(from = 1950, to = 2010, by = 10)
-#world.pop <- c(2525779, 3026003, 3691173, 4449049, 5320817, 6127700, 6916183)
-#UNpop <- as.data.frame(cbind(world.pop, year))
+year <- seq(from = 1950, to = 2010, by = 10)
+world.pop <- c(2525779, 3026003, 3691173, 4449049, 5320817, 6127700, 6916183)
+UNpop <- as.data.frame(cbind(world.pop, year))
 
-#x <- sample(1:100, 10)
+x <- sample(1:100, 10)
